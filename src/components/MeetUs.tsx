cda1351721763c103acc79b95afe5550db49117c
steadman1 <steadman@vt.edu>
2024-08-22 23:11:55 -0400

    modified the meet us scroll locking

--- src/components/MeetUs.tsx
+++ src/components/MeetUs.tsx
@@ -6,31 +6,45 @@ function MeetUs() {
     const employees = Employee.employees;
 
     useEffect(() => {
-        const handleScroll = (e: Event) => {
+        const handleScroll = () => {
+
+            const entrance = document.getElementById("meet-us-entrance");
+            if (!entrance) return;
+
+            if (entrance.getBoundingClientRect().top <= 0) {
+                window.scrollTo({ top: entrance.offsetTop, behavior: "smooth" });
+                setLockScroll(_ => true);
+            } else {
+                setLockScroll(_ => false);
+            }
+        }
+        
+        const handleWheelTouchMove = (e: Event) => {
             const entrance = document.getElementById("meet-us-entrance");
             if (!entrance) return;
             
-            if (entrance.getBoundingClientRect().top <= 0) {
-                console.log(e);
+            if (entrance.getBoundingClientRect().top <= 0 || lockScroll) {
                 e.preventDefault();
                 e.stopPropagation();
-
+                e.stopImmediatePropagation();
+            
                 if (!lockScroll) {
                     window.scrollTo({ top: entrance.offsetTop, behavior: "smooth" });
-
                     setLockScroll(_ => true);
                 }
             }
         }
 
-        window.addEventListener("wheel", handleScroll, { passive: false });
-        window.addEventListener("touchmove", handleScroll, { passive: false });
+        window.addEventListener("scroll", handleScroll, { passive: false });
+        window.addEventListener("wheel", handleWheelTouchMove, { passive: false });
+        window.addEventListener("touchmove", handleWheelTouchMove, { passive: false });
 
         return () => {
-            window.addEventListener("wheel", handleScroll, { passive: false });
-            window.addEventListener("touchmove", handleScroll, { passive: false });
+            window.removeEventListener("scroll", handleScroll, { passive: false });
+            window.removeEventListener("wheel", handleWheelTouchMove, { passive: false });
+            window.removeEventListener("touchmove", handleWheelTouchMove, { passive: false });
         }
-    }, []);
+    }, [lockScroll]);
 
     return (
         <>
